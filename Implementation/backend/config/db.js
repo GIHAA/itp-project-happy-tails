bb34b0c5fca47eb4c7294c101628ffd99d1b42d8
GIHAA <gihansudeeparandimal@gmail.com>
2023-03-15 12:59:39 +0530

    Db connection logic added

---
+++ Implementation/backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://itp:itp@cluster0.o3gmnsw.mongodb.net/?retryWrites=true&w=majority')
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
