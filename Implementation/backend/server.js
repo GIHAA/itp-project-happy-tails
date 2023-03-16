ace93f1184eaa98a643158d974caea9923e920cc
IT21181856 <it21181856@my.sliit.lk>
2023-03-16 08:44:52 +0530

    Added server route

--- Implementation/backend/server.js
+++ Implementation/backend/server.js
@@ -16,6 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/suppliers/', supplierRoutes)
 
 app.use(errorHandler)
 
