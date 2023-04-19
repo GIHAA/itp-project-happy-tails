9a5780501f06e1e78ed998238c7072cca706ec6e
IT21181856 <it21181856@my.sliit.lk>
2023-04-19 15:07:09 +0530

    routes are added

--- Implementation/frontend/src/App.js
+++ Implementation/frontend/src/App.js
@@ -18,6 +18,15 @@ import Events from './components/User/Events';
 import Pets from './components/User/myPets';
 import Profile from './components/User/myProfile';
 
+import SupplierList from './components/SupplierList';
+import AddSuppliers from './components/AddSuppliers';
+import ManageSuppliers from './components/ManageSuppliers';
+import UpdateSupplier from './components/UpdateSupplier';
+import StockRequests from './components/StockRequests';
+import StockBudgetRequests from './components/StockBudgetRequests'
+import StockBudgetRequestForm from './components/StockBudgetRequestForm';
+import UpdateStockBudgetRequest from './components/UpdateStockBudgetRequest';
+
 
 import Spinner from './components/common/Spinner';
 
@@ -37,11 +46,22 @@ function App() {
             
 
             //user
-            <Route path='user' element={<UserPortal />} />
+              <Route path='user' element={<UserPortal />} />
               <Route path='profile' element={<Profile />} />
               <Route path='booking' element={<Booking />} />
               <Route path='pets' element={<Pets />} />
               <Route path='events' element={<Events />} />
+
+             
+              //
+            <Route path='/supplierList' element={<SupplierList />} />
+            <Route path='/addSuppliers' element={<AddSuppliers />} />
+            <Route path='/manageSuppliers' element={<ManageSuppliers />} />
+            <Route path='/updateSuppliers/:id' element={<UpdateSupplier />} />
+            <Route path='/stockRequests' element={<StockRequests />} />
+            <Route path='/StockBudgetRequests' element={<StockBudgetRequests />} />
+            <Route path='/StockBudgetRequestForm' element={<StockBudgetRequestForm />} />
+            <Route path='/UpdateStockBudgetRequest/:id' element={<UpdateStockBudgetRequest />} />
 
 
               <Route path='test' element={<Spinner />} />
