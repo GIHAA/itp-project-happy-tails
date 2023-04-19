f1983a995470c5d60d12868f01a709222a02e776
Nipun Ayodya <it21187278@my.sliit.lk>
2023-04-19 19:26:55 +0530

    PaymentAdded

---
+++ Implementation/backend/models/addPaymentModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const paymentSchema = mongoose.Schema(
+  {
+    cus_id: {
+      type: String,
+      required: [true, 'Please add a cus_id'],
+     
+    },
+    pet_id: {
+      type: String,
+      required: [true, 'Please add a pet_id'],
+    
+    },
+    payment: {
+      type: Number,
+      required: [true, 'Please add a payment'],
+    },
+
+    status: {
+      type: String,
+      enum: ['Verified', 'CANCELED', 'PAID', 'FINISHED'],
+      default: 'PAID',
+      required: [true, 'Please add a status'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Payment', paymentSchema)
---
+++ Implementation/backend/routes/addPaymentRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const {
+
+    addPayment,
+    readPayment,
+    updatePayment,
+    deletePayment,
+    readAllPayment,
+    
+    
+} = require('../controller/paymentController')
+
+const { protect, userProtect, adminProtect } = require('../middleware/authMiddleware')
+const { paymentValidation } = require('../middleware/cusDonaMiddleware')
+
+router.post('/',paymentValidation ,addPayment)
+router.get('/:id', readPayment)
+router.get('/', readAllPayment)
+router.put('/:id',paymentValidation, updatePayment)
+router.delete('/:id', deletePayment)
+
+
+module.exports = router
