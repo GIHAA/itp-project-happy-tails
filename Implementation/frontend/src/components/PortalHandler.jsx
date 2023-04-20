0796c87e732e50450eecd91ff0f161a651d56c44
GIHAA <gihansudeeparandimal@gmail.com>
2023-04-20 07:26:56 +0530

    porttal handler

--- Implementation/frontend/src/components/PortalHandler.jsx
+++ Implementation/frontend/src/components/PortalHandler.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import bg from "../assets/Image.png";
@@ -7,63 +7,74 @@ import { Link } from "react-router-dom";
 import Home from "./Home";
 import FinaDashBoard from "./Finance_Management/FinaDashBoard";
 import Dashboard from "./Animal_Management/Dashboard/Dashboard";
-import InvDashboard from "./InvDashboard";
-import EventDashboard from  "./EventManagement/EventDashboard";
-import SupplierList  from "./SupplierList";
+import InvDashboard from "./InvDashboard";
+import EventDashboard from "./EventManagement/EventDashboard";
+import SupplierList from "./SupplierList";
 import VHome from "./VHome";
 import Header from "./common/PortalHeader";
 import PortalHeader from "./common/PortalHeader";
 
-
 function PortalHandler() {
-
   const { user } = useSelector((state) => state.auth);
-
 
   const renderPortals = (param) => {
     switch (param) {
-      case 'ADMIN':
+      case "ADMIN":
         return <></>;
-      case 'USER':
-        return <><Home/></>;
-      case 'EVENT_MANAGER':
-        return <>
-        <PortalHeader/>
-        <EventDashboard/>
-        </>;
-      case 'INVENTORY_MANAGER':
-        return <>
-                <PortalHeader/>
-        <InvDashboard/></>;
-      case 'VEHICLE_MANAGER':
-        return <>
-                <PortalHeader/>
-        <VHome/> </>;
-      case 'ANIMAL_MANAGER':
-        return <>
-                <PortalHeader/>
-        <Dashboard/> </>;
-      case 'FINANCIAL_MANAGER':
-        return <>
-                <PortalHeader/>
-        <FinaDashBoard/></>;
-      case 'SUPPLIER_MANAGER':
-        return <>
-                <PortalHeader/>
-        <SupplierList/></>;
+      case "USER":
+        return (
+          <>
+            <Home />
+          </>
+        );
+      case "EVENT_MANAGER":
+        return (
+          <>
+            <PortalHeader />
+            <EventDashboard />
+          </>
+        );
+      case "INVENTORY_MANAGER":
+        return (
+          <>
+            <PortalHeader />
+            <InvDashboard />
+          </>
+        );
+      case "VEHICLE_MANAGER":
+        return (
+          <>
+            <PortalHeader />
+            <VHome />{" "}
+          </>
+        );
+      case "ANIMAL_MANAGER":
+        return (
+          <>
+            <PortalHeader />
+            <Dashboard />{" "}
+          </>
+        );
+      case "FINANCIAL_MANAGER":
+        return (
+          <>
+            <PortalHeader />
+            <FinaDashBoard />
+          </>
+        );
+      case "SUPPLIER_MANAGER":
+        return (
+          <>
+            <PortalHeader />
+            <SupplierList />
+          </>
+        );
       default:
         return <></>;
     }
   };
-  
 
-  return (
-    <>
-      {
-        user ? renderPortals(user.role) : <Home/>
-      }
-    </>
-  );
+  return <>{user ? renderPortals(user.role) : <Home />}</>;
 }
 
 export default PortalHandler;
