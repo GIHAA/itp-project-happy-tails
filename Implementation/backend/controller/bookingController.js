070a3a870caaa1bb630183075693d2902726fe61
GIHAA <gihansudeeparandimal@gmail.com>
2023-04-30 16:19:05 +0530

    Route protection

--- Implementation/backend/controller/bookingController.js
+++ Implementation/backend/controller/bookingController.js
@@ -31,6 +31,12 @@ const addBooking = asyncHandler(async (req, res) => {
     : res.status(400).json({ message: "Booking not created" });
 });
 
+const readBookingOpen = asyncHandler(async (req, res) => {
+
+  const booking = await Booking.find({});
+  res.json(booking);
+});
+
 const readBooking = asyncHandler(async (req, res) => {
 
   const id = req.params.id;
@@ -38,10 +44,7 @@ const readBooking = asyncHandler(async (req, res) => {
   res.json(booking);
 });
 
-const readUserBooking = asyncHandler(async (req, res) => {
-  const booking = await Booking.find({ cus_id: req.body._id });
-  res.json(booking);
-});
+
 
 const updateBooking = asyncHandler(async (req, res) => {
   const { id, description, startDate, endDate, status } = req.body;
@@ -57,6 +60,9 @@ const updateBooking = asyncHandler(async (req, res) => {
     : res.status(400).json({ message: "Booking not updated" });
 });
 
+
+
+
 const deleteBooking = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const booking = await Booking.findByIdAndDelete(id);
@@ -71,5 +77,6 @@ module.exports = {
   readBooking,
   updateBooking,
   deleteBooking,
-  readUserBooking,
+  readBookingOpen
 };
+
