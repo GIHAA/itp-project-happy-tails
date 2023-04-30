24770efc753ae5de5d8d26640890ea0bc9765956
GIHAA <gihansudeeparandimal@gmail.com>
2023-04-30 16:22:22 +0530

    resolve conficts

--- Implementation/frontend/src/components/VSideBar.jsx
+++ Implementation/frontend/src/components/VSideBar.jsx
@@ -19,58 +19,6 @@ function VSideBar() {
 
   return (
     <div className=" bg-[#FF9F00] h-[100vh] flex-[15%] sticky top-0">
-<<<<<<< HEAD
-      
-      
-    <div>
-      <img src={logo} alt="logo" className=" w-[150px] h-[100px] mx-auto"></img>
-      <h3 className=" text-[#2E4960] font-bold text-l text-center w-[150px] leading-5 my-2 tracking-wide mx-auto">
-        VEHICLE MANAGEMENT
-      </h3>
-    </div>
-
-    
-    <div className="my-6 ">
-
-      
-
-        <NavLink to='/vhome' activeClassName =" bg-[#797979]"
-        className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >HOME</NavLink>
-
-
-        <NavLink to='/vehicledashboard' activeClassName =" bg-[#797979]"
-        className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >DASHBOARD</NavLink>
-
-
-        <NavLink to='/vehicles' activeClassName =" bg-[#797979]"
-        className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >VEHICLES</NavLink>
-
-
-        <NavLink to='/availability' activeClassName=" bg-[#797979]"
-        className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >VEHICLE MAINTENANCE</NavLink>
-
-
-        <NavLink to='/pending' activeClassName =" bg-[#797979]"
-        className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >BOOKING REQUESTS </NavLink>
-
-        <NavLink to='/vbudgets' activeClassName =" bg-[#797979]"
-        className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >BUDGET REQUESTS </NavLink>
-
-<div style={{ marginTop: '50px' }}>
-    <button onClick={onLogout}
-        className="flex items-center px-5 py-1 ml-12 mr-5 bg-[#ffffff] text-[#2E4960] shadow-lg font-semibold hover:bg-[#818181] hover:text-white rounded-xl">
-        <img src={logoutimg} alt="Logout" className="w-4 h-4 mr-2" />
-        <span>Logout</span>
-    </button>
-</div>
-
-=======
       <div>
         <img
           src={logo}
@@ -81,7 +29,6 @@ function VSideBar() {
           VEHICLE MANAGEMENT
         </h3>
       </div>
->>>>>>> 735415bdc6eb34bc6e06195684fd1681ae098d8a
 
       <div className=" ">
         <NavLink
