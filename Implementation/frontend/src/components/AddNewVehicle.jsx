a7ee1c8237106d3794f152dc26a690c502a598d1
IT21181924 <IT21181924@my.sliit.lk>
2023-04-30 16:45:10 +0530

    merge conflict

--- Implementation/frontend/src/components/AddNewVehicle.jsx
+++ Implementation/frontend/src/components/AddNewVehicle.jsx
@@ -1,66 +1,43 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import bgimg from "../assets/bgimg.jpg";
-import axios from "axios";
+import { Link } from 'react-router-dom';
+import bgimg from "../assets/bgimg.jpg"
+import axios from 'axios'
 import VSideBar from "./VSideBar";
 
-<<<<<<< HEAD
 
-
-
-
-=======
->>>>>>> 735415bdc6eb34bc6e06195684fd1681ae098d8a
 export default function AddNewVehicle() {
-  const [plateNo, setPlateNo] = useState("");
-  const [driverId, setDriverId] = useState("");
-  const [agentId, setAgentId] = useState("");
-  const [vModel, setVModel] = useState("");
-  const [insuranceExpirationDate, setInsuranceExpirationDate] = useState("");
 
-<<<<<<< HEAD
     const[plateNo, setPlateNo] = useState("");
-    const[driverId, setDriverId] = useState("");
-    const[agentId, setAgentId] = useState("");
     const[vModel, setVModel] = useState("");
+    const[availability, setAvailability] = useState("");
     const[insuranceExpirationDate, setInsuranceExpirationDate] = useState("");
-    const[status, setStatus] = useState("");
-
 
     function addVehicle(e) {
         e.preventDefault();
-      
-        axios.get("http://localhost:8080/api/vehicle").then((response) => {
-          const vehicles = response.data;
-      
-          const isPlateNumberDuplicate = vehicles.some(
-            (vehicle) => vehicle.plateNo === plateNo
-          );
-      
-          if (isPlateNumberDuplicate) {
-            alert("Cannot insert !! Vehicle with same plate number already exists !!");
-          } else {
-            const newVehicle = {
-              plateNo,
-              driverId,
-              agentId,
-              vModel,
-              insuranceExpirationDate,
-              status,
-            };
-      
-            axios
-              .post("http://localhost:8080/api/vehicle", newVehicle)
-              .then(() => {
-                alert("Vehicle added");
-              })
-              .catch((err) => {
-                alert(`Vehicle insert unsuccessful ${err}`);
-              });
-          }
-        });
-      }
-      
+
+        const newVehicle = {
+
+      plateNo,
+      availability,
+      vModel,
+      insuranceExpirationDate
+
+    }
+
+
+        axios.post("http://localhost:8080/api/vehicle",newVehicle)
+        .then(()=>{
+            alert("Vehicle added")
+
+       }).catch((err)=>{
+            if(err.response.status === 409)
+                alert("Cannot insert !! vehicle already exists !!")
+            else
+                alert(`Vehicle insert unsuccessful ${err}`)
+       })
+
+
+    }
 
     return (
        //Main container
@@ -77,54 +54,17 @@ export default function AddNewVehicle() {
               <div className=" flex p-5">
 
               <Link to='/addnvehicle' 
-=======
-  function addVehicle(e) {
-    e.preventDefault();
-
-    const newVehicle = {
-      plateNo,
-      driverId,
-      agentId,
-      vModel,
-      insuranceExpirationDate,
-    };
-
-    axios
-      .post("http://localhost:8080/api/vehicle", newVehicle)
-      .then(() => {
-        alert("Vehicle added");
-      })
-      .catch((err) => {
-        if (err.response.status === 409)
-          alert("Cannot insert !! vehicle already exists !!");
-        else alert(`Vehicle insert unsuccessful ${err}`);
-      });
-  }
-
-  return (
-    //Main container
-    <div className="flex scroll-smooth">
-      <VSideBar />
-      {/*Right Side container start*/}
-      <div className="bg-[#d9d9d9] flex-[85%]">
-        {/*Header Part*/}
-        <div className="bg-[#2E4960] h-100 w-full">
-          <h1 className="text-white font-bold text-3xl leading-5 tracking-wide pt-5 pl-5 ">
-            ADD VEHICLE
-          </h1>
-
-          <div className=" flex p-5">
-            <Link
-              to="/addnvehicle"
->>>>>>> 735415bdc6eb34bc6e06195684fd1681ae098d8a
               className=" bg-[#797979] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[12px] block w-[100px] text-center mr-2"
-            >
-              +ADD
-            </Link>
-          </div>
-        </div>
+              >+ADD</Link> 
 
-<<<<<<< HEAD
+              </div>
+          </div>
+  
+          {/*Body Part*/}
+          <div 
+            style={{ backgroundImage: `url(${bgimg})` }}
+            className="bg-cover bg-center h-screen w-full fixed" >
+
                 <div className=" bg-white bg-opacity-90 w-[75%] h-[80%] absolute top-5 left-[80px] overflow-scroll"> 
 
                     <div className="w-[800px] h-[400px] mx-auto rounded-2xl bg-white mt-8">
@@ -147,6 +87,12 @@ export default function AddNewVehicle() {
                                             setPlateNo(e.target.value)}} required />                        
                                     </div>
 
+                                    
+                                </div>
+
+                                <div className="flex mb-6">
+
+
                                     <div className=" w-[50%]  ">
                                         <label className="">Vehicle Model :</label>
                                         <input type="text" 
@@ -155,10 +101,15 @@ export default function AddNewVehicle() {
                                             setVModel(e.target.value)}} required />                        
                                     </div>
 
-                                    
-                                </div>
+                                    <div className=" w-[50%]  ">
+                                        <label className="">Availability :</label>
+                                        <input type="text" 
+                                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]" 
+                                        onChange={(e)=>{
+                                            setAvailability(e.target.value)}} required />                        
+                                    </div>
 
-                                
+                                </div>
 
                                 <div className="flex mb-6">
 
@@ -169,15 +120,6 @@ export default function AddNewVehicle() {
                                         onChange={(e)=>{
                                             setInsuranceExpirationDate(e.target.value)}} required />                        
                                     </div>
-
-                                    <div className=" w-[50%]  ">
-                                        <label className="">Availability :</label>
-                                        <input type="text" 
-                                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]" 
-                                        onChange={(e)=>{
-                                          setStatus(e.target.value)}} required />                        
-                                    </div>
-
 
                                     
                                 </div>
@@ -194,106 +136,15 @@ export default function AddNewVehicle() {
                                 </div>
                         </form>
                         </div>
-=======
-        {/*Body Part*/}
-        <div
-          style={{ backgroundImage: `url(${bgimg})` }}
-          className="bg-cover bg-center h-screen w-full fixed"
-        >
-          <div className=" bg-white bg-opacity-90 w-[75%] h-[80%] absolute top-5 left-[80px] overflow-scroll">
-            <div className="w-[800px] h-[400px] mx-auto rounded-2xl bg-white mt-8">
-              <h1 className=" text-[#ffffff] bg-[#FF9F00] rounded-t-2xl font-bold text-3xl h-20 mb-4 pt-5 text-center drop-shadow-md">
-                Enter Vehicle Details
-              </h1>
->>>>>>> 735415bdc6eb34bc6e06195684fd1681ae098d8a
 
-              <div className=" pl-5">
-                <form className="mx-auto" onSubmit={addVehicle}>
-                  <div className="flex mb-6">
-                    <div>
-                      <label className="">Plate Number :</label>
-                      <input
-                        type="text"
-                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]"
-                        onChange={(e) => {
-                          setPlateNo(e.target.value);
-                        }}
-                        required
-                      />
                     </div>
-
-                    <div>
-                      <label className="">Driver ID :</label>
-                      <input
-                        type="text"
-                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]"
-                        onChange={(e) => {
-                          setDriverId(e.target.value);
-                        }}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex mb-6">
-                    <div className=" w-[50%]  ">
-                      <label className="">Agent ID :</label>
-                      <input
-                        type="text"
-                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]"
-                        onChange={(e) => {
-                          setAgentId(e.target.value);
-                        }}
-                        required
-                      />
-                    </div>
-
-                    <div className=" w-[50%]  ">
-                      <label className="">Vehicle Model :</label>
-                      <input
-                        type="text"
-                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]"
-                        onChange={(e) => {
-                          setVModel(e.target.value);
-                        }}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex mb-6">
-                    <div className=" w-[50%]  ">
-                      <label className="">Insurance Expiration Date :</label>
-                      <input
-                        type="text"
-                        className=" rounded-3xl py-2.5 px-5 w-[50vh] text-sm text-gray-900 bg-[#E4EBF7] border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-[#FF9F00]"
-                        onChange={(e) => {
-                          setInsuranceExpirationDate(e.target.value);
-                        }}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex mt-24 h-10">
-                    <button className="text-white bg-[#FF9F00] hover:bg-[#2E4960] focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-3xl text-l sm:w-auto px-20 py-5.5 text-center ml-[100px]">
-                      <Link to="/vehicles">Cancel</Link>
-                    </button>
-
-                    <button
-                      type="submit"
-                      className="text-white bg-[#FF9F00] hover:bg-[#2E4960] focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-3xl text-l sm:w-auto px-20 py-5.5 text-center ml-[130px]"
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
+                   
+                </div>
+    
           </div>
-        </div>
-      </div>{" "}
-      {/*Right Side container end*/}
-    </div> //Main container end
-  );
+
+        </div> {/*Right Side container end*/}
+      </div> //Main container end
+
+    )
 }
--- Implementation/frontend/src/components/VSideBar.jsx
+++ Implementation/frontend/src/components/VSideBar.jsx
@@ -1,25 +1,14 @@
-import React from "react";
-import logo from "../assets/logo.png";
-import v from "../assets/v.png";
-import { Link, NavLink } from "react-router-dom";
-import { logout, reset } from "../services/auth/authSlice";
-import logoutimg from "../assets/logout.png";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import logo from "../assets/logo.png"
+import v from "../assets/v.png"
+import { Link, NavLink } from 'react-router-dom';
+
+
 
 function VSideBar() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const onLogout = () => {
-    dispatch(logout());
-    dispatch(reset());
-    navigate("/");
-  };
-
   return (
+
     <div className=" bg-[#FF9F00] h-[100vh] flex-[15%] sticky top-0">
-<<<<<<< HEAD
       
       
     <div>
@@ -62,94 +51,18 @@ function VSideBar() {
         className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
         >BUDGET REQUESTS </NavLink>
 
-<div style={{ marginTop: '50px' }}>
-    <button onClick={onLogout}
-        className="flex items-center px-5 py-1 ml-12 mr-5 bg-[#ffffff] text-[#2E4960] shadow-lg font-semibold hover:bg-[#818181] hover:text-white rounded-xl">
-        <img src={logoutimg} alt="Logout" className="w-4 h-4 mr-2" />
-        <span>Logout</span>
-    </button>
+        
+
+    </div>
+
+    <div>
+      <hr className="mt-[80px] mb-[15px] border-[#2E4960] w-40 mx-auto"></hr>
+      <img src={v} className=" w-[140px] h-[140px] mx-auto"></img>
+    </div>
+  
 </div>
-
-=======
-      <div>
-        <img
-          src={logo}
-          alt="logo"
-          className=" w-[150px] h-[100px] mx-auto"
-        ></img>
-        <h3 className=" text-[#2E4960] font-bold text-l text-center w-[150px] leading-5 my-2 tracking-wide mx-auto">
-          VEHICLE MANAGEMENT
-        </h3>
-      </div>
->>>>>>> 735415bdc6eb34bc6e06195684fd1681ae098d8a
-
-      <div className=" ">
-        <NavLink
-          to="/vhome"
-          activeClassName=" bg-[#797979]"
-          className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >
-          HOME
-        </NavLink>
-
-        <NavLink
-          to="/vehicledashboard"
-          activeClassName=" bg-[#797979]"
-          className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >
-          DASHBOARD
-        </NavLink>
-
-        <NavLink
-          to="/vehicles"
-          activeClassName=" bg-[#797979]"
-          className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >
-          VEHICLES
-        </NavLink>
-
-        <NavLink
-          to="/availability"
-          activeClassName=" bg-[#797979]"
-          className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >
-          VEHICLE MAINTENANCE
-        </NavLink>
-
-        <NavLink
-          to="/pending"
-          activeClassName=" bg-[#797979]"
-          className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >
-          BOOKING REQUESTS{" "}
-        </NavLink>
-
-        <NavLink
-          to="/vbudgets"
-          activeClassName=" bg-[#797979]"
-          className="link bg-[#2E4960] px-[15px] py-[8px] rounded-[120px] font-bold text-white text-[10px] block w-[150px] text-center mb-7 mx-auto"
-        >
-          BUDGET REQUESTS{" "}
-        </NavLink>
-
-        <div>
-          <button
-            onClick={onLogout}
-            className="flex items-center px-5 py-1 ml-12 mr-5  bg-[#ffffff] text-[#2E4960] shadow-lg font-semibold hover:bg-[#818181] hover:text-white rounded-xl"
-          >
-            <img src={logoutimg} alt="Logout" className="w-4 h-4 mr-2" />
-            <span>Logout</span>
-          </button>
-        </div>
-
-
-      <div>
-        <hr className="mt-[20px] mb-[15px] border-[#2E4960] w-40 mx-auto"></hr>
-        <img src={v} className=" w-[120px] h-[100px] mx-auto"></img>
-      </div>
-    </div>
-    </div>
-  );
+   
+  )
 }
 
 export default VSideBar;
