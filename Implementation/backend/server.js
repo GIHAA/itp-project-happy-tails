f5ee4a6af28deeec0dcc50458dcab119aacd63a8
GIHAA <gihansudeeparandimal@gmail.com>
2023-05-01 12:29:14 +0530

    wip

--- Implementation/backend/server.js
+++ Implementation/backend/server.js
@@ -31,6 +31,8 @@ app.use("/api/users", require("./routes/userRoutes.js"));
 app.use("/api/booking/", require("./routes/bookingRoutes.js"));
 app.use("/api/sendEmail/", require("./routes/sendEmailRoutes.js"));
 app.use("/api/userspets", require("./routes/usersPetsRoutes"));
+app.use('/api/feedback', require('./routes/feedBackRoutes'))
+
 
 app.use("/api/vehicle/", require("./routes/vehicleRoutes.js"));
 app.use("/api/transport/", require("./routes/transportRoutes.js"));
---
+++ Implementation/frontend/src/services/api/feedBack.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/feedback/";
+
+const view = async (userData) => {
+    const response = await axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${userData.token}`,
+      },
+    });
+  
+    return response.data;
+  };
+
+const add = async (userData) => {
+    const response = await axios.post(API_URL , userData, {
+      headers: {
+        Authorization: `Bearer ${userData.token}`,
+      },
+    });
+  
+    return response.data;
+  };
+
+const update = async (userData) => {
+    const response = await axios.put(API_URL + userData._id, {
+      headers: {
+        Authorization: `Bearer ${userData.token}`,
+      },
+    });
+  
+    return response.data;
+  };
+
+const deletefeedback = async (userData) => {
+  const response = await axios.delete (API_URL + userData._id, {
+    headers: {
+      Authorization: `Bearer ${userData.token}`,
+    },
+  });
+
+  return response.data;
+};
+
+const feedbackServices = {
+    view,
+    add,
+    update,
+    deletefeedback,
+}
+
+export default feedbackServices;
